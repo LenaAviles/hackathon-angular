566f2799137fc72c974c90dcb6611252d0240630
Olena Ricabal Aviles <Olena_Ricabal_Aviles@epam.com>
2018-04-03 16:42:36 +0300

    added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA  } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { PrimengModule } from './primeng.module';
+import { SharedModule } from './shared/shared.module';
 
 import { QuestionService } from './services/question.service';
 import { CategoryService } from './services/category.service';
@@ -19,13 +17,11 @@ import { QuestionsComponent } from './questions/questions.component';
 import { QuestionDetailComponent } from './question-detail/question-detail.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
-import { MenuComponent } from './menu/menu.component';
+
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { HeaderComponent } from './shared/navigation/header/header.component';
 
 import { environment } from '../environments/environment';
-
 
 @NgModule({
   declarations: [
@@ -33,23 +29,18 @@ import { environment } from '../environments/environment';
     QuestionsComponent,
     QuestionDetailComponent,
     CategoriesComponent,
-    AddQuestionComponent,
-    MenuComponent,
+    AddQuestionComponent,   
     LoginComponent,
-    SignupComponent, 
-    HeaderComponent,     
+    SignupComponent,      
   ],
   imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule,
+    BrowserModule,    
     AppRoutingModule,    
     BrowserAnimationsModule,    
-    ReactiveFormsModule, 
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule,  
-    PrimengModule,    
+    AngularFireAuthModule,     
+    SharedModule   
   ],
   providers: [QuestionService, CategoryService, DoctorService, AuthService],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,29 @@
-// import { NgModule } from '@angular/core';
-// import { HeaderComponent } from './navigation/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PrimengModule } from '../primeng.module';
 
-// @NgModule({
-//   imports: [
-//     HeaderComponent
-//   ],
-//   exports: [
-//     HeaderComponent
-//   ],
-//   declarations: [HeaderComponent]
-// })
-// export class SharedModule { }
+import { HeaderComponent } from './navigation/header/header.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimengModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimengModule,
+
+    HeaderComponent,
+    MenuComponent
+  ],
+  declarations: [HeaderComponent, MenuComponent]
+})
+export class SharedModule { }
